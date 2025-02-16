041fc01634963095c2dfb908c4fec82c75fbb177
Littleberryjam <littlebrryjam@gmail.com>
2025-02-16 22:33:01 +0800

    Added click sound, gave clickareas functionality

--- game.js
+++ game.js
@@ -1,10 +1,18 @@
 let gameCanvas = document.getElementById("gameCanvas");
 let ctx = gameCanvas.getContext("2d");
-let img = document.getElementById("background");
-let cursorimg = document.getElementById("cursor");
 
 
-ctx.drawImage(img, 0, 0);
+
+let backgroundImg = document.getElementById("background");
+let cursorImg = new Image();
+cursorImg.src = "resources/images/cursor.png";
+
+let clickSound = new Audio("resources/sounds/click.wav");
+
+
+
+
+
 
 class clickableArea{
     constructor(x, y, width, height){
@@ -16,16 +24,34 @@ class clickableArea{
     draw(){
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
+    clicked(x, y){
+        if(x < this.x || x > this.x+this.width){
+            return false;
+        }
+        if(y < this.y || y > this.y+this.height){
+            return false;
+        }
+        return true;
+    }
 }
 
 var test = new clickableArea(50, 50, 100, 100);
+ctx.drawImage(backgroundImg, 0, 0);
 test.draw();
 
 rect = gameCanvas.getBoundingClientRect();
-addEventListener("mousemove", function(e){
+gameCanvas.addEventListener("mousemove", function(e){
     let mX = e.clientX - rect.x;
     let mY = e.clientY - rect.y;
-    ctx.drawImage(img, 0, 0);
+    ctx.drawImage(backgroundImg, 0, 0);
     test.draw();
-    ctx.drawImage(cursorimg, mX-cursorimg.width, mY);
-})
+    ctx.drawImage(cursorImg, mX-cursorImg.width, mY);
+});
+gameCanvas.addEventListener("click", function(e){
+    let mX = e.clientX - rect.x;
+    let mY = e.clientY - rect.y;
+    if(test.clicked(mX, mY)){
+        clickSound.play();
+    }
+    
+});
