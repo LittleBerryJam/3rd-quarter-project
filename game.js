66510ed5d4d3c00e6110a2040dca5d2320fb8634
Littleberryjam <littlebrryjam@gmail.com>
2025-03-08 23:15:30 +0800

    preload images

--- game.js
+++ game.js
@@ -342,6 +342,22 @@ points.pickUp.imgs = ["resources/images/pick up.png", "resources/images/pick up 
 points.ignore.imgs = ["resources/images/ignore.png", "resources/images/ignore 2.png"];
 points.pickLock.imgs = ["resources/images/pick lock 1.png", "resources/images/pick lock 2.png"];
 
+function preloadImage(url){
+    const img = new Image();
+    img.src = url;
+}
+for(var __point of Object.keys(points)){
+    _point = points.__point;
+    if(_point){
+        if(_point.img == ""){
+            for(let url = 0; url<_point.imgs.length; url++){
+                preloadImage(url);
+            }
+        }else{
+            preloadImage(_point.img);
+        }
+    }
+}
 
 //choices is [choicePoint, choiceText]
 points.start.choices = [{_point: points.accept, text: "Accept the offer"}, {_point: points.reject, text: "Reject the offer"}];
